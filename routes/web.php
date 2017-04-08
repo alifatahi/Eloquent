40cf8a58de61c2d6047907d8861d017b781051de
Ali Fatahi <a.fatahi1992@gmail.com>
2017-04-08 23:28:47 +0430

    Finsih Project

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,37 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::all();
+
+    return View::make('welcome')->with('tasks',$tasks);
+});
+
+Route::get('/tasks/{task_id?}',function($task_id){
+    $task = Task::find($task_id);
+
+    return Response::json($task);
+});
+
+Route::post('/tasks',function(Request $request){
+    $task = Task::create($request->all());
+
+    return Response::json($task);
+});
+
+Route::put('/tasks/{task_id?}',function(Request $request,$task_id){
+    $task = Task::find($task_id);
+
+    $task->task = $request->task;
+    $task->description = $request->description;
+
+    $task->save();
+
+    return Response::json($task);
+});
+
+Route::delete('/tasks/{task_id?}',function($task_id){
+    $task = Task::destroy($task_id);
+
+    return Response::json($task);
 });
